423e71fcaa8c6dd7ce68b88a1e97355b48c1695f
Pauline Nalwanga <paulinon141@gmail.com>
2022-09-13 20:58:29 +0300

    start wagtail and install it

---
+++ lifecoach/lifecoach/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-82#axp)*_m#47te&8q6!he@on8f4^@j7t!_de+jq0++@j2*(33"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
