d6f474485b6f3329d38140b833c74db9a7b7f20b
Pauline Nalwanga <paulinon141@gmail.com>
2022-09-14 16:14:39 +0300

    Add wagtail and start project lifecoah

--- lifecoach/settings/dev.py
+++ lifecoach/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-82#axp)*_m#47te&8q6!he@on8f4^@j7t!_de+jq0++@j2*(33"
+SECRET_KEY = "django-insecure-wl^6zm4nm%g9k-$&+=6cf+y_!&^=i$t!@8m=jav=4e^rnm-y%k"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
